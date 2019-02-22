5905ec9976effc860ea039bcbae9a41e19915259
Roshan Kanel <rkanel@busways.com.au>
2019-02-22 18:03:47 +1100

    React Navigation

---
+++ components/MainComponent.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import Menu from './MenuComponent';
+import {DISHES} from '../shared/dishes';
+import Dishdetail from './DishDetailComponent';
+import {View, Platform} from 'react-native';
+import {createStackNavigator} from 'react-navigation';
+
+const MenuNavigator = createStackNavigator({
+    Menu: { screen: Menu},
+    Dishdetail:{ screen: Dishdetail}
+}, {
+    initialRouteName: 'Menu', 
+    navigationOptions: {
+        headerStyle:{
+            backgroundColor: '#512DA8'
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle:{
+            color:'#fff'
+        }
+    }
+})
+
+class Main extends Component{
+    render(){
+        return(
+            <View style={{flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}> 
+                <MenuNavigator />
+            </View>
+        );
+    }
+}
+
+export default Main;
